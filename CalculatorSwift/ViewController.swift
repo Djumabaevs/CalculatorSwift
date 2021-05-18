0d684da2b754b32f9a5f324da42acda3a77a181a
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-18 07:57:17 +0600

    Initial Commit

---
+++ CalculatorSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorSwift
+//
+//  Created by Bakyt Dzhumabaev on 18/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
